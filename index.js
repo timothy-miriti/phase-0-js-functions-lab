3eddf39d99cb947477d7e832c08f2c7fd4ab3325
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-03-29 15:21:00 +0300

    add function1,function2,function3,function4 and function5 to js file

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+console.log(calculateTax(5000))
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+console.log(convertToUpperCase("timseymiriti"))
 
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+console.log(findMaximum(830,406))
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+console.log(isPalindrome("tim"))
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
+console.log(calculateDiscountedPrice(1000,10))
 
 
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };// Function 1: calculateTax
