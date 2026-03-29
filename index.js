79c7a5bcb2a096d20615d00d8fb3d7b43ec0e27a
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-03-29 15:38:08 +0300

    update the js file

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-// Function 1: calculateTax
 function calculateTax(amount) {
   return amount * 0.10;
 }
-console.log(calculateTax(5000))
-// Function 2: convertToUpperCase
+
 function convertToUpperCase(text) {
   return text.toUpperCase();
 }
-console.log(convertToUpperCase("timseymiriti"))
 
-// Function 3: findMaximum
 function findMaximum(num1, num2) {
   return num1 > num2 ? num1 : num2;
 }
-console.log(findMaximum(830,406))
 
-// Function 4: isPalindrome
 function isPalindrome(word) {
-  const reversed = word.split('').reverse().join('');
-  return word === reversed;
+  // Handle empty strings or single characters as true per your test requirements
+  if (word === "") return true;
+  
+  const cleanWord = word.toLowerCase();
+  const reversed = cleanWord.split('').reverse().join('');
+  return cleanWord === reversed;
 }
-console.log(isPalindrome("tim"))
 
-// Function 5: calculateDiscountedPrice
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
-  const discount = originalPrice * (discountPercentage / 100);
-  return originalPrice - discount;
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
 }
-console.log(calculateDiscountedPrice(1000,10))
 
-
-
-
-// This is required for the test to function properly  
+// CRITICAL: This allows the test suite to find your functions
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+}; 
 //module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };// Function 1: calculateTax
